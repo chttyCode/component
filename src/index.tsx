6e8ba212a10cb7e3b77ee36eed627eaf513cfd18
chtty <631486004@qq.com>
2021-06-20 08:00:17 +0800

    test husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,27 @@
-// import React from "react";
-// import ReactDom from "react-dom";
+import React from "react";
+import ReactDom from "react-dom";
 
-// function rootConfig(target: typeof App){
-//   target.role = 'root'
-// }
+function rootConfig(target: typeof App) {
+  target.role = "root";
+}
 
-// @rootConfig
-// class App extends React.Component {
-//   static role: string;
-//   state={
-//     number:0
-//   }
-//   handlerClick=()=>{
-//     this.setState({number:this.state.number+1})
-//   }
-//   render(){
-//     return  <>
-//     <div>Hello React typescript {this.state.number}</div>
-//     <button onClick={this.handlerClick}> +</button>
-//     </>
-//   }
-// }
+@rootConfig
+class App extends React.Component {
+  static role: string;
+  state = {
+    number: 0,
+  };
+  handlerClick = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  render() {
+    return (
+      <>
+        <div>Hello React typescript {this.state.number}</div>
+        <button onClick={this.handlerClick}> +</button>
+      </>
+    );
+  }
+}
 
-// ReactDom.render(<App />, document.getElementById("root"));
-
-const p = new Promise((resolve, reject) => {
-  resolve(true);
-});
-console.log(p);
+ReactDom.render(<App />, document.getElementById("root"));
