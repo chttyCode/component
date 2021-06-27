8840271035a2c44df2b2a28a8881f49725b55bd0
chtty <631486004@qq.com>
2021-06-27 09:17:16 +0800

    remove

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
-
-export { default as Button } from "./components/button";
-export { default as Input } from "./components/input";
-export { default as Icon } from "./components/icon";
-export { default as Menu } from "./components/menu";
